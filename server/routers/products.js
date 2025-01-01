e5ddf20797fb60c997f3fca2b1e11890e570aabb
axxe40 <valih.frenato@gmail.com>
2025-01-02 03:00:52 +0700

    refactor product router

--- server/routers/products.js
+++ server/routers/products.js
@@ -2,6 +2,5 @@ const Controller = require("../controllers/HomePageController");
 const routerProducts = require("express").Router();
 
 routerProducts.get("/", Controller.showAllGuitars)
-routerProducts.get("/recommendation", Controller.recommendation)
 
 module.exports = routerProducts
